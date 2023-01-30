31af6e050da0f49c5f38f34bf1359acadbf35e82
Danika Enad <danika.enad@gmail.com>
2023-01-29 16:10:19 -0800

    to do list app created

--- App.js
+++ App.js
@@ -1,22 +1,55 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/header'
+import TodoItem from './components/todoItem'
+import AddTodo from './components/addTodo';
 
 export default function App() {
-  const [name, setName] = useState("shaun")
 
-const clickHandler = () => {
-  setName('chun-li')
-}
+  const [todos, setTodos] = useState([
+    {text: 'buy coffee', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'play on the switch', key: '3'},
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+
+    if (text.length > 3){
+      setTodos((prevTodos) => {
+        return [
+          { text:text, key:Math.random().toString()},
+          ...prevTodos
+        ]
+      })
+    } else {
+      Alert.alert('OOPS!', "todos must be mover 3 chars long!", [{text: 'Understood', onPress: () => console.log('alert closed')}])
+    }
+
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text></Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler}/>
+    <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList 
+              data={todos}
+              renderItem={({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -24,10 +57,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  buttonContainer: {
-    marginTop: 20
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 40,
   }
 });
